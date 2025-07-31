eb65b041b9b0d535743dbcaf6d6b7c71c03ac88a
육도연 <yookdoyeon@yugdoyeon-ui-MacBookPro.local>
2025-07-31 20:51:02 +0900

    Initial commit with entire algorithm_sol folder

---
+++ conditional_p/2525.py
@@ -0,0 +1,48 @@
+# h, m = map(int, input().split())
+# t=int(input())
+# if m>=t:
+#     print(h, m-t)
+# else:
+#     if h==0:
+#         print(23,60-(t-m))
+#         if t>60:
+#             a=int(t/60)
+#             h=h-a
+#             b=t%60
+#             if h-a<0:
+#                 c=a-h
+#                 print(23-c,b)
+#             if m>=b:
+#                 print(h, b)
+#             else:
+#                 print(h-1, 60-(b-m))
+#     else:
+#         print(h-1, 60-(t-m))
+
+
+def ad(h,m,t):
+    if h==0:
+        print(23,60-(t-m))
+    else :
+        print(23+h,t)
+        
+
+
+h, m = map(int, input().split())
+t=int(input())
+if m>=t:
+    print(h, m-t)
+else: 
+    if t>60:
+        a=int(t/60)
+        h=h-a
+        t=t%60
+        if m>=t:
+            print(h, t)
+            if h<=0:
+                ad(h,m,t)
+            else:
+                print(h-1, 60-(t-m))
+    else:   
+        print(h-1, 60-(t-m))
+    
---
+++ conditional_p/2884.py
@@ -0,0 +1,9 @@
+#입력하는 방식을 확인해야함
+h, m = map(int, input().split())
+if m>=45:
+    print(h, m-45)
+else:
+    if h==0:
+        print(23, m+15)
+    else:
+        print(h-1, m+15)
