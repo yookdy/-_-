28bb556cc35b28c2c5e9ae87dbb5bd9c2f7f2997
육도연 <yookdoyeon@yugdoyeon-ui-MacBookPro.local>
2025-08-07 23:23:23 +0900

    repost a file

--- 1D_arrangement/10813.py
+++ 1D_arrangement/10813.py
@@ -1,3 +1,5 @@
+#baekjoon one dimensional ordering 10813 8/4
+#두 수를 연속으로 받고 리스트의 길이만큼 1부터 n까지의 숫자를 순서대로 넣고 매개값을 이용해서 i-1,j-1번째 인덱스 값을 서로 바꾸는 방식으로 구현
 n,m=map(int, input().split())
 list1=[0]*n
 for x in range(n):
